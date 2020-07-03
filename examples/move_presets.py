2b22062c97f374cd2e534fd89a6bcfe659c2f39e
Evgeny <evgenytikhonov@outlook.com>
2020-07-02 22:15:28 -0400

    add move_preset, snapshot

--- examples/move_presets.py
+++ examples/move_presets.py
@@ -3,6 +3,8 @@ import asyncio, sys
 from onvif import ONVIFCamera
 import cv2
 from skimage import io
+import numpy as np
+import urllib
 
 IP="192.168.1.108"   # Camera IP address
 PORT=80           # Port
@@ -82,6 +84,12 @@ def setup_move():
     # YMAX = ptz_configuration_options.Spaces.ContinuousPanTiltVelocitySpace[0].YRange.Max
     # YMIN = ptz_configuration_options.Spaces.ContinuousPanTiltVelocitySpace[0].YRange.Min
 
+def url_to_image(url):
+    resp = urllib.urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    return image
+
 
 class CameraController:
     presets = []
@@ -117,9 +125,12 @@ class CameraController:
 
         snapshot = media.GetSnapshotUri({'ProfileToken': profileToken})
         print('snapshot uri {}'.format(snapshot))
-        image = io.imread(snapshot)
-        n_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        cv2.imwrite('./image1.jpg', n_image)
+        # image = io.imread(snapshot)
+        # n_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # cv2.imwrite('./image1.jpg', n_image)
+
+        image = url_to_image(snapshot)
+        cv2.imwrite('./image2.jpg', image)
 
         return current_prest, self.status.MoveStatus.PanTilt, snapshot
 
