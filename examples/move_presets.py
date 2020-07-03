64afc03989dca70e8c28ccc3db8f6af9d64ff518
Evgeny <evgenytikhonov@outlook.com>
2020-07-02 21:16:57 -0400

    add move_preset, snapshot

--- examples/move_presets.py
+++ examples/move_presets.py
@@ -1,6 +1,8 @@
 import zeep
 import asyncio, sys
 from onvif import ONVIFCamera
+import cv2
+from skimage import io
 
 IP="192.168.1.108"   # Camera IP address
 PORT=80           # Port
@@ -50,7 +52,7 @@ def setup_move():
     # GetStatus
     print("GetStatus")
     status = ptz.GetStatus({'ProfileToken': profileToken})
-    print('status {} {} {}   ? => '.format(status.Position.PanTilt.x, status.Position.PanTilt.y,
+    print('status {} {} {}   ? => {}'.format(status.Position.PanTilt.x, status.Position.PanTilt.y,
                                       status.Position.Zoom.x,
                                    status.MoveStatus.PanTilt))
 
@@ -81,5 +83,78 @@ def setup_move():
     # YMIN = ptz_configuration_options.Spaces.ContinuousPanTiltVelocitySpace[0].YRange.Min
 
 
+class CameraController:
+    presets = []
+    status = None
+
+    def get_current_preset(self):
+        mycam = ONVIFCamera(IP, PORT, USER, PASS)
+        # Create media service object
+        media = mycam.create_media_service()
+        print("setup_move {} {}", mycam, media)
+        # Create ptz service object
+
+        ptz = mycam.create_ptz_service()
+        # Get target profile
+        media_profile = media.GetProfiles()[0]
+        profileToken = media_profile.token
+
+        # GetStatus
+        print("GetStatus")
+        self.status = ptz.GetStatus({'ProfileToken': profileToken})
+        print('status {} {} {}   ? => {}'.format(self.status.Position.PanTilt.x, self.status.Position.PanTilt.y,
+                                                 self.status.Position.Zoom.x,
+                                                 self.status.MoveStatus.PanTilt))
+
+        min_dist = 100
+        current_prest = None
+        for preset in self.presets:
+            position = preset['PTZPosition']
+            dist = pow((self.status.Position.PanTilt.x - position.PanTilt.x), 2) + pow((self.status.Position.PanTilt.y - position.PanTilt.y), 2)
+            if dist < min_dist:
+                min_dist = dist
+                current_prest = preset
+
+        snapshot = media.GetSnapshotUri({'ProfileToken': profileToken})
+        print('snapshot uri {}'.format(snapshot))
+        image = io.imread(snapshot)
+        n_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        cv2.imwrite('./image1.jpg', n_image)
+
+        return current_prest, self.status.MoveStatus.PanTilt, snapshot
+
+    def get_presets(self):
+        mycam = ONVIFCamera(IP, PASS, USER, )
+        # Create media service object
+        media = mycam.create_media_service()
+        print("setup_move {} {}", mycam, media)
+        # Create ptz service object
+
+        ptz = mycam.create_ptz_service()
+        # Get target profile
+        media_profile = media.GetProfiles()[0]
+        profileToken = media_profile.token
+
+        # Get presets
+        print("Get Presets...")
+        gp = ptz.create_type('GetPresets')
+        gp.ProfileToken = profileToken
+        self.presets = ptz.GetPresets(gp)
+        for preset in self.presets:
+            if (hasattr(preset, "Name")):
+                name = preset.Name
+            else:
+                name = ""
+            position = preset['PTZPosition']
+
+            print("preset {} => ({}, {}, {})".format(name, position.PanTilt.x,
+                                                 position.PanTilt.y,
+                                                 position.Zoom.x))
+        return self.presets
+
+
 if __name__ == '__main__':
-    setup_move()
+    # setup_move()
+    camera = CameraController()
+    camera.get_presets()
+    camera.get_current_preset()
